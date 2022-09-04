4bf44831b2daf2765d2ac5191813e90de7b2d5d7
Solene Rapenne <solene@perso.pw>
2022-09-04 02:29:50 +0200

    utils: bento systemd timer

---
+++ utils/bento.nix
@@ -0,0 +1,24 @@
+{ lib, pkgs, ... }:
+let
+  timer = "*:0/5";
+in
+{
+  systemd.timers.bento-upgrade = {
+    enable = true;
+    timerConfig = {
+      OnCalendar = "${timer}";
+      Unit = "bento-upgrade.service";
+    };
+    wantedBy = [ "timers.target" ];
+  };
+
+  systemd.services.bento-upgrade = {
+    enable = true;
+    path = with pkgs; [ openssh git nixos-rebuild ];
+    serviceConfig.Type = "oneshot";
+    script = ''
+      cd /var/bento
+      /bin/sh update.sh
+    '';
+  };
+}
