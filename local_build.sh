ce7893f439279519c11eb15f033bb5daf23d5fef
Solene Rapenne <solene@perso.pw>
2022-09-05 18:37:40 +0200

    consistent formatting

--- local_build.sh
+++ local_build.sh
@@ -10,18 +10,18 @@ fi
 if [ "$COMMAND" = "switch" ] || [ "$COMMAND" = "test" ]
 then
 
-  # we only allow these commands if you have only one name
-  if [ -n "$NAME" ]
-  then
-    SUDO="sudo"
-    echo "you are about to $COMMAND $NAME, are you sure? Ctrl+C to abort"
-    read a
-  else
-    echo "you can't use $COMMAND without giving a single configuration to use with variable NAME"
-  fi
+    # we only allow these commands if you have only one name
+    if [ -n "$NAME" ]
+    then
+        SUDO="sudo"
+        echo "you are about to $COMMAND $NAME, are you sure? Ctrl+C to abort"
+        read a
+    else
+        echo "you can't use $COMMAND without giving a single configuration to use with variable NAME"
+    fi
 
 else # not using switch or test
-  SUDO=""
+    SUDO=""
 fi
 
 cd hosts
@@ -29,33 +29,32 @@ cd hosts
 # load all hosts or the one defined in environment variable NAME
 if [ -z "$NAME" ]
 then
-  NAME=*
+    NAME=*
 fi
 
 SUCCESS=0
 for i in $NAME
 do
-  if test -d "$i"
-  then
-    TMP="$(mktemp -d /tmp/bento-build.XXXXXXXXXXXX)"
-    TMPLOG="$(mktemp /tmp/bento-build-log.XXXXXXXXXXXX)"
-    rsync -aL "$i/" "$TMP/"
-
-    printf "${COMMAND} ${i}: "
-
-    if test -f "$i/flake.nix"
+    if test -d "$i"
     then
-        cd "$TMP" || exit 5
-        # add files to a git repo
-        test -d .git || git init >/dev/null 2>/dev/null
-        git add . >/dev/null
-        $SUDO nixos-rebuild "${COMMAND}" --flake .#bento-machine 2>${TMPLOG} >${TMPLOG}
-        if [ $? -eq 0 ]; then echo "success"  ; else echo "failure" ; SUCCESS=$(( SUCCESS + 1 )) ; cat ${TMPLOG} ; fi
+        TMP="$(mktemp -d /tmp/bento-build.XXXXXXXXXXXX)"
+        TMPLOG="$(mktemp /tmp/bento-build-log.XXXXXXXXXXXX)"
+        rsync -aL "$i/" "$TMP/"
 
-    else
-        cd "$TMP" || exit 5
-        $SUDO nixos-rebuild "${COMMAND}" --no-flake -I nixos-config="$TMP/configuration.nix" 2>${TMPLOG} >${TMPLOG}
-        if [ $? -eq 0 ]; then echo "success"  ; else echo "failure" ; SUCCESS=$(( SUCCESS + 1 )) ; cat ${TMPLOG} ; fi
+        printf "${COMMAND} ${i}: "
+
+        if test -f "$i/flake.nix"
+        then
+            cd "$TMP" || exit 5
+            # add files to a git repo
+            test -d .git || git init >/dev/null 2>/dev/null
+            git add . >/dev/null
+            $SUDO nixos-rebuild "${COMMAND}" --flake .#bento-machine 2>${TMPLOG} >${TMPLOG}
+            if [ $? -eq 0 ]; then echo "success"  ; else echo "failure" ; SUCCESS=$(( SUCCESS + 1 )) ; cat ${TMPLOG} ; fi
+        else
+            cd "$TMP" || exit 5
+            $SUDO nixos-rebuild "${COMMAND}" --no-flake -I nixos-config="$TMP/configuration.nix" 2>${TMPLOG} >${TMPLOG}
+            if [ $? -eq 0 ]; then echo "success"  ; else echo "failure" ; SUCCESS=$(( SUCCESS + 1 )) ; cat ${TMPLOG} ; fi
     fi
     cd - >/dev/null || exit 5
     rm -fr "$TMP"
