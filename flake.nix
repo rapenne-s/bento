e684890b200d6ab5e710f12373d0f9d7e0d71eb8
Solene Rapenne <solene@perso.pw>
2022-09-10 00:11:42 +0200

    add flakes

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "bento: an asynchronous NixOS deployment tool";
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.default =
+      with import nixpkgs { system = "x86_64-linux"; };
+      stdenv.mkDerivation {
+        name = "bento";
+        src = self;
+        version = "1.0.1";
+
+        installPhase = ''
+          mkdir -p $out/bin
+          mkdir -p $out/share
+          install -Dm555 bento $out/bin/
+          install -Dm444 fleet.nix $out/share/
+          install -Dm444 config.sh.sample $out/share/
+          install -Dm444 LICENSE $out/share/
+          install -Dm444 README.md $out/share/
+          install -Dm444 utils/bento.nix $out/share/
+        '';
+      };
+  };
+}
