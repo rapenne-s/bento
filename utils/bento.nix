2392b7720f772c6cce711edf47eda9308a404bcf
Solene Rapenne <solene.rapenne@tweag.io>
2022-10-06 20:52:11 +0200

    utils: use startAt in bento-upgrade service to auto create the timer

--- utils/bento.nix
+++ utils/bento.nix
@@ -5,18 +5,9 @@
 }: let
   timer = "*:0/15";
 in {
-  systemd.timers.bento-upgrade = {
-    enable = true;
-    timerConfig = {
-      OnCalendar = "${timer}";
-      Unit = "bento-upgrade.service";
-    };
-    wantedBy = ["timers.target"];
-    after = ["network-online.target"];
-  };
-
   systemd.services.bento-upgrade = {
     enable = true;
+    startAt = "${timer}";
     path = with pkgs; [openssh git nixos-rebuild nix gzip];
     serviceConfig.Type = "oneshot";
     script = ''
