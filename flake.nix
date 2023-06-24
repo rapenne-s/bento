6418bd64ecabbfa720c7a87ec39ee1a8b46a907e
Solène Rapenne <solene@perso.pw>
2023-06-24 11:46:41 +0200

    bento: use flake-utils to support all systems

--- flake.nix
+++ flake.nix
@@ -2,30 +2,36 @@
   description = "bento: an asynchronous NixOS deployment tool";
 
   inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }: {
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.default = pkgs.stdenv.mkDerivation {
+          name = "bento";
+          src = self;
 
-    packages.x86_64-linux.default =
-      with import nixpkgs { system = "x86_64-linux"; };
-
-      stdenv.mkDerivation {
-        name = "bento";
-        src = self;
-
-        patchPhase = ''
+          patchPhase = ''
             substituteInPlace bento --replace 'inotifywait' "${pkgs.inotify-tools}/bin/inotifywait";
-        '';
+          '';
 
-        installPhase = ''
-          mkdir -p $out/bin
-          mkdir -p $out/share
-          install -Dm555 bento $out/bin/
-          install -Dm444 fleet.nix $out/share/
-          install -Dm444 config.sh.sample $out/share/
-          install -Dm444 LICENSE $out/share/
-          install -Dm444 README.md $out/share/
-          install -Dm444 utils/bento.nix $out/share/
-        '';
-      };
-  };
+          installPhase = ''
+            mkdir -p $out/bin
+            mkdir -p $out/share
+            install -Dm555 bento $out/bin/
+            install -Dm444 fleet.nix $out/share/
+            install -Dm444 config.sh.sample $out/share/
+            install -Dm444 LICENSE $out/share/
+            install -Dm444 README.md $out/share/
+            install -Dm444 utils/bento.nix $out/share/
+          '';
+        };
+      }
+    );
 }
